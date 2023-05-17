d48e4468ee79ef6f3e26f44911efa941b90e40ab
Niruifedi <ifediniruizuchukwu@gmail.com>
2023-05-17 15:41:01 +0100

    app control

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const AppController = require('../controllers/AppController').default;
+const AppController = require('../controllers/AppController');
 const StudentsController = require('../controllers/StudentsController');
 
 const router = express.Router();
