f82b87d660e1f4ed05251ee0acc69175bb10538a
Niruifedi <ifediniruizuchukwu@gmail.com>
2023-05-19 10:02:40 +0100

    fix

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,6 @@
-const assert = require('assert')
-const calculateNumber = require('./0-calcul')
+const assert = require("assert");
+const { it, describe } = require("mocha");
+const calculateNumber = require("./1-calcul");
 
 describe("calculateNumber", function () {
     it("checking if numbers round", function() {
