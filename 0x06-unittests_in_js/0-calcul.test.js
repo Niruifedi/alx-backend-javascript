b2a6e392b776c97c2b52507940f35b0746ae7d01
Niruifedi <ifediniruizuchukwu@gmail.com>
2023-05-19 04:19:26 +0100

    strict assert

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,21 +3,24 @@ const calculateNumber = require('./0-calcul')
 
 describe("calculateNumber", function () {
     it("checking if numbers round", function() {
-        assert.equal(calculateNumber(1, 3), 4);
+        assert.strictEqual(calculateNumber(1, 3), 4);
     });
     it("checking if numbers round", function() {
-        assert.equal(calculateNumber(1, 3.7), 5);
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
     });
     it("checking if numbers round", function() {
-        assert.equal(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     });
     it("checking if numbers round", function() {
-        assert.equal(calculateNumber(1.5, 3.7), 6);
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
     it("checking zero", function() {
         assert.strictEqual(calculateNumber(0, 0), 0);
     });
     it("checking negative values", function() {
         assert.strictEqual(calculateNumber(-1.3, -4.5), -5);
+    });
+    it(`checking if numbers round`, function() {
+      assert.strictEqual(calculateNumber(-1.4, -1.3), -2);
     });
 });
